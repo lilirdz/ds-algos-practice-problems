371e6e2ad9b9b4bc779781a1513d2658f8c45faa
lilirdz <lilimrod@gmail.com>
2020-11-16 15:50:53 -0600

    added fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+// Create a for loop that iterates up to 100 
+// while outputting "fizz" at multiples of 3, 
+// "buzz" at multiples of 5 
+// and "fizzbuzz" at multiples of 3 and 5
+
+
+for (let i = 0; i < 100; i++){
+    if (i % 15 === 0){
+        console.log( i + "fizzbuzz")
+    }if (i % 3 === 0){
+        console.log(i + "fizz")
+    }if( i % 5 === 0){
+        console.log(i + "buzz")
+    }else{
+        console.log(i)
+    }
+}
